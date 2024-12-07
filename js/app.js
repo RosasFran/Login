e2fe78a21a1b450b1aee483325d3f9e6ed0d5f5e
Franco <francorosas415@gmail.com>
2024-12-06 23:21:11 -0300

    Se agrega el proyecto

---
+++ js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const inputUsername = document.querySelector('#username');
+    const inputPassword = document.querySelector('#password')
+})
